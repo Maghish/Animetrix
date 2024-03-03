8d96cf82d089bf44e49c5b72dd0b7dd8e7d205d4
Maghish <mail4mithran@gmail.com>
2024-03-03 21:21:05 +0530

    Edited lint + Added index.js (main file)

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const token = process.env.TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
