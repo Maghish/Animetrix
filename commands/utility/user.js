bf1889d14172058fcadf064bf672cd4eb537ad21
Maghish <mail4mithran@gmail.com>
2024-04-12 19:38:40 +0530

    Deleted user.js file

--- commands/utility/user.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("1").setDescription("1"),
-
-  async execute(interaction) {
-    await interaction.reply("1");
-  },
-};
