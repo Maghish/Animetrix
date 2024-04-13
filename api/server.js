8e236dc4b4ca35f9de686b0eda1ab1a3e72e61eb
Maghish <mail4mithran@gmail.com>
2024-04-13 13:53:32 +0530

    Created API routes

--- api/server.js
+++ api/server.js
@@ -3,22 +3,25 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const apiRoutes = require("./routes/api.routes");
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ 
-  origin: "*"
-}))
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello" });
-});
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("/api", apiRoutes);
 
 mongoose.connect(process.env.MONGOURI).then(() => {
   console.log("Connected to MongoDB Successfully");
   app.listen(process.env.PORT, () => {
     console.log("Listening on port " + process.env.PORT);
   });
-})
+});
