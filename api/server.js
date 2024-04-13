8c0bc4b26bc6f8879b7322903e63195868d1e636
Maghish <mail4mithran@gmail.com>
2024-04-13 13:47:55 +0530

    Added mongoose

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("listening on port " + process.env.PORT);
-});
+mongoose.connect(process.env.MONGOURI).then(() => {
+  console.log("Connected to MongoDB Successfully");
+  app.listen(process.env.PORT, () => {
+    console.log("Listening on port " + process.env.PORT);
+  });
+})
