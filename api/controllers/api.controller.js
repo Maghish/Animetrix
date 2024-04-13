73c8608e71302ad4117272741f848b951e568f99
Maghish <mail4mithran@gmail.com>
2024-04-13 14:13:32 +0530

    Fixed a minor bug

--- api/controllers/api.controller.js
+++ api/controllers/api.controller.js
@@ -1,5 +1,6 @@
 const pokemon = require("pokemontcgsdk");
 const dotenv = require("dotenv");
+const CardModel = require("../models/cardModel");
 
 dotenv.config();
 
@@ -17,102 +18,108 @@ async function getCardsThroughSet(setName) {
 }
 
 async function migrate(req, res) {
-  const { set } = req.body;
+  try {
+    const { set } = req.body;
 
-  const res = await getCardsThroughSet(set);
+    const allCards = await getCardsThroughSet(set);
 
-  // ** This part needs to be changed **
+    // ** This part needs to be changed **
 
-  res.data.forEach(async (card) => {
-    /* 
+    allCards.data.forEach(async (card) => {
+      /* 
         Some of the fields in the Pokemon TCG API Card object might be presennt such as listed below.
         So all of the if condition blocks will check if the card object has that field and if so it will update the variables below,
         if there is no field in the card object, then it will be empty to be a placeholder in the Card Model.
       */
 
-    let evolvesFrom = "";
-    let evolvesTo = [];
-    let rules = [];
-    let abilities = [];
-    let attacks = [];
-    let weaknesses = [];
-    let resistances = [];
-    let retreatCost = [];
-    let flavorText = [];
+      let evolvesFrom = "";
+      let evolvesTo = [];
+      let rules = [];
+      let abilities = [];
+      let attacks = [];
+      let weaknesses = [];
+      let resistances = [];
+      let retreatCost = [];
+      let flavorText = [];
 
-    if (card.evolvesFrom) {
-      evolvesFrom = card.evolvesFrom;
-    }
+      if (card.evolvesFrom) {
+        evolvesFrom = card.evolvesFrom;
+      }
 
-    if (card.evolvesTo) {
-      evolvesTo = card.evolvesTo;
-    }
+      if (card.evolvesTo) {
+        evolvesTo = card.evolvesTo;
+      }
 
-    if (card.rules) {
-      rules = card.rules;
-    }
+      if (card.rules) {
+        rules = card.rules;
+      }
 
-    if (card.abilities) {
-      card.abilities.forEach((ability) => {
-        abilities.push({
-          name: ability.name,
-          text: ability.text,
-          type: ability.type,
-          functionID: "",
+      if (card.abilities) {
+        card.abilities.forEach((ability) => {
+          abilities.push({
+            name: ability.name,
+            text: ability.text,
+            type: ability.type,
+            functionID: "",
+          });
         });
-      });
-    }
+      }
 
-    if (card.attacks) {
-      card.attacks.forEach((move) => {
-        attacks.push({
-          name: move.name,
-          text: move.text,
-          damage: move.damage,
-          cost: move.cost,
-          functionID: "",
+      if (card.attacks) {
+        card.attacks.forEach((move) => {
+          attacks.push({
+            name: move.name,
+            text: move.text,
+            damage: move.damage,
+            cost: move.cost,
+            functionID: "",
+          });
         });
+      }
+
+      if (card.weaknesses) {
+        weaknesses = card.weaknesses;
+      }
+
+      if (card.resistances) {
+        resistances = card.resistances;
+      }
+
+      if (card.retreatCost) {
+        retreatCost = card.retreatCost;
+      }
+
+      if (card.flavorText) {
+        flavorText = card.flavorText;
+      }
+
+      const newCard = new CardModel({
+        id: card.id,
+        name: card.name,
+        supertype: card.supertype,
+        subtypes: card.subtypes,
+        hp: card.hp,
+        types: card.types,
+        evolvesFrom: evolvesFrom,
+        evolvesTo: evolvesTo,
+        rules: rules,
+        abilities: abilities,
+        attacks: attacks,
+        weaknesses: weaknesses,
+        resistances: resistances,
+        retreatCost: retreatCost,
+        rarity: card.rarity,
+        flavorText: card.flavorText,
+        image: card.images.large,
       });
-    }
 
-    if (card.weaknesses) {
-      weaknesses = card.weaknesses;
-    }
-
-    if (card.resistances) {
-      resistances = card.resistances;
-    }
-
-    if (card.retreatCost) {
-      retreatCost = card.retreatCost;
-    }
-
-    if (card.flavorText) {
-      flavorText = card.flavorText;
-    }
-
-    const newCard = new CardModel({
-      id: card.id,
-      name: card.name,
-      supertype: card.supertype,
-      subtypes: card.subtypes,
-      hp: card.hp,
-      types: card.types,
-      evolvesFrom: evolvesFrom,
-      evolvesTo: evolvesTo,
-      rules: rules,
-      abilities: abilities,
-      attacks: attacks,
-      weaknesses: weaknesses,
-      resistances: resistances,
-      retreatCost: retreatCost,
-      rarity: card.rarity,
-      flavorText: card.flavorText,
-      image: card.images.large,
+      await newCard.save();
     });
 
-    await newCard.save();
-  });
+    return res.status(200).json({ message: "Success" });
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
 }
 
 module.exports = {
