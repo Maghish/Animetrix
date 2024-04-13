09cbb891f4c628b53f000f64dc511b4ea1afe630
Maghish <mail4mithran@gmail.com>
2024-04-13 21:51:43 +0530

    Made find command

---
+++ app/commands/utility/find.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("find")
+    .setDescription("Find a pokemon card")
+    .addStringOption((option) =>
+      option
+        .setName("pokemon")
+        .setDescription("Enter a pokemon name to search")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const cardName = interaction.options.getString("pokemon");
+
+    try {
+      const APIURL =  `${process.env.APIURL}api/getcard`;
+      const res = await axios.post(APIURL, {
+        cardName: cardName
+      })
+
+      const embed = new EmbedBuilder()
+        .setTitle(res.data.card[0].name)
+        .setImage(res.data.card[0].image)
+
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      await interaction.reply("Error!");
+      console.error(error);
+    }
+  },
+};
