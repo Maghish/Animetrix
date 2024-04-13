cdff853b573491b7a3965cb1d5651080c35d8ffd
Maghish <mail4mithran@gmail.com>
2024-04-13 14:13:23 +0530

    Import migrate function properly

--- api/routes/api.routes.js
+++ api/routes/api.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const migrate = require("../controllers/api.controller");
+const { migrate } = require("../controllers/api.controller");
 
 const router = express.Router();
 
