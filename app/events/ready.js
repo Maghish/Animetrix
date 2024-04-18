136eaafc2072123b6fc529506409b94bf63a2240
Maghish <mail4mithran@gmail.com>
2024-04-18 13:55:07 +0530

    Created global loop with Nanotechpikachu

--- app/events/ready.js
+++ app/events/ready.js
@@ -1,6 +1,7 @@
 const { Events } = require("discord.js");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { PermissionsBitField } = require('discord.js');
 
 dotenv.config();
 
@@ -15,5 +16,26 @@ module.exports = {
     if (mongoose.connect) {
       console.log("Connected to MongoDB");
     }
+    setInterval(() => {
+      client.guilds.cache.forEach((guild) => {
+        // Get an array of text channels where the bot can send messages
+        const textChannels = guild.channels.cache
+          .filter(
+            (channel) =>
+              channel.type === 0 &&
+              channel
+                .permissionsFor(client.user)
+                .has(PermissionsBitField.Flags.SendMessages)
+          )
+          .map((channel) => channel);
+
+        // Select a random channel
+        if (textChannels.length > 0) {
+          const randomIndex = Math.floor(Math.random() * textChannels.length);
+          const randomChannel = textChannels[randomIndex];
+          randomChannel.send("hi").catch(console.error);
+        }
+      });
+    }, 60000); // Repeat every minute
   },
 };
