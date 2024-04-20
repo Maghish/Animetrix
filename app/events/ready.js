48fe2b34a9bc5a42b418fc5587007fd1d795f9ba
Maghish <mail4mithran@gmail.com>
2024-04-20 10:49:43 +0530

    Send message for every 10 minutes

--- app/events/ready.js
+++ app/events/ready.js
@@ -1,7 +1,7 @@
 const { Events } = require("discord.js");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const { PermissionsBitField } = require('discord.js');
+const { PermissionsBitField } = require("discord.js");
 
 dotenv.config();
 
@@ -17,25 +17,25 @@ module.exports = {
       console.log("Connected to MongoDB");
     }
     setInterval(() => {
-      client.guilds.cache.forEach((guild) => {
-        // Get an array of text channels where the bot can send messages
-        const textChannels = guild.channels.cache
-          .filter(
-            (channel) =>
-              channel.type === 0 &&
-              channel
-                .permissionsFor(client.user)
-                .has(PermissionsBitField.Flags.SendMessages)
-          )
-          .map((channel) => channel);
+      try {
+        client.guilds.cache.forEach((guild) => {
+          const textChannels = guild.channels.cache
+            .filter(
+              (channel) =>
+                channel.type === 0 &&
+                channel
+                  .permissionsFor(client.user)
+                  .has(PermissionsBitField.Flags.SendMessages)
+            )
+            .map((channel) => channel);
 
-        // Select a random channel
-        if (textChannels.length > 0) {
-          const randomIndex = Math.floor(Math.random() * textChannels.length);
-          const randomChannel = textChannels[randomIndex];
-          randomChannel.send("hi").catch(console.error);
-        }
-      });
-    }, 60000); // Repeat every minute
+          if (textChannels.length > 0) {
+            const randomIndex = Math.floor(Math.random() * textChannels.length);
+            const randomChannel = textChannels[randomIndex];
+            randomChannel.send("hi").catch(console.error);
+          }
+        });
+      } catch (error) {}
+    }, 600000);
   },
 };
